31bba2e741f4dc69519389d47c80157ed984a4f5
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-11-28 17:06:49 +0500

    JS

---
+++ main.js
@@ -0,0 +1,55 @@
+// 1 ---------------------------------------------------------------------------- 1
+// let word = 'hello';
+// let result = '';
+
+// for (let i = 0; i <= word.length - 1; i++) {
+//     result += word[i];
+//     if (i < word.length - 1) {
+//         result += ', ';
+//     }
+// }
+
+// console.log(result);
+
+// 2 -----------------------------------------------------------------------------2
+// let text = "abc253"
+// let sum = 0
+
+// for (let i = 0; i < text.length; i++) {
+//     let char = text[i]
+//     if (char >= "0" && char <= "9") {
+//         sum += Number(char)
+//     }
+// }
+
+// console.log(sum)
+
+// 3 -----------------------------------------------------------------------------3
+// let word = 'script'
+// let res = ''
+
+// for (let i = 0; i <= word.length - 1; i++) {
+//     res += (word[i] + word[i]);
+// }
+
+// console.log(res);
+
+// 4 ----------------------------------------------------------------------------4
+// let word1 = "abc"
+// let word2 = "cba"
+// let res = ""
+// let res2 = ""
+
+// for (let i = word1.length - 1; i >= 0; i--) {
+//     res += word1[i]
+// }
+
+// for (let i = 0; i <= word2.length - 1; i++) {
+//     res2 += word2[i]
+// }
+
+// if (res === res2) {
+//     console.log("true")
+// }else {
+//     console.log('false');
+// }
